0efe30a2d799880c8adc5bbc06dfbf6428e335be
abhip <apardhi20@iitk.ac.in>
2023-01-09 22:48:45 +0530

    Done useState

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Samarth",
+    age: "19",
+    message: "Crazy boi",
+  });
+  const changeMessage = () => {
+    let newPerson = person;
+    newPerson.message = "Crazy bhyi";
+    setPerson({ ...person, message: "Crazy bhyi" });
+  };
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [counter, setCounter] = useState(0);
+  const changeCounter = (val) => {
+    setCounter(counter + val);
+  };
+  const changeCounterDelayed = () => {
+    setTimeout(() => {
+      // setCounter(counter + 1);
+      setCounter((prevState) => {
+        return prevState + 1;
+      });
+    }, 2000);
+  };
+  return (
+    <>
+      <h2>regular counter</h2>
+      <h1>{counter}</h1>
+      <button className="btn" onClick={() => changeCounter(1)}>
+        increase
+      </button>
+      <button className="btn" onClick={() => changeCounter(-1)}>
+        decrease
+      </button>
+      <button className="btn" onClick={() => setCounter(0)}>
+        reset
+      </button>
+      <h2>complex counter</h2>
+      <h1>{counter}</h1>
+      <button className="btn" onClick={changeCounterDelayed}>
+        delayed increase
+      </button>
+    </>
+  );
 };
 
 export default UseStateCounter;
