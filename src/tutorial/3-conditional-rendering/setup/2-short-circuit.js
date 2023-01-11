01d53e62213aa93fcbf3fb538752a150c6df4380
abhip <apardhi20@iitk.ac.in>
2023-01-11 17:23:32 +0530

    cond-rendering and forms done

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [isError, setIsError] = useState(false);
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      <h1>abhishek pardhi</h1>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {!isError ? <h1>no error</h1> : <h1>Error...</h1>}
+    </>
+  );
 };
 
 export default ShortCircuit;
