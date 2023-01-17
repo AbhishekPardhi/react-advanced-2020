e2aa5470159ff700ff9634550170562403b3b5ea
abhip <apardhi20@iitk.ac.in>
2023-01-17 16:33:09 +0530

    useRef useReducer prop-drilling useContext

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/4-forms/setup/2-multiple-inputs";
+import Setup from "./tutorial/8-useContext/setup/1-context-api";
 import Final from "./tutorial/1-useState/final/1-error-example";
 
 function App() {
